186e631d111c4121f6626963f4348e2cb09dc248
nikamp-24 <nikamp870@gmail.com>
2025-12-13 18:41:21 +0530

    layouts practice

--- Week1_UI_Basics/layouts_practice/app/src/main/java/com/poojanikam/layouts_practice/MainActivity.java
+++ Week1_UI_Basics/layouts_practice/app/src/main/java/com/poojanikam/layouts_practice/MainActivity.java
@@ -1,15 +1,20 @@
 package com.poojanikam.layouts_practice;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.activity.ComponentActivity;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends ComponentActivity {
+Button btnLinearLayout, btnRelativeLayout, btnConstraintLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,35 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnLinearLayout = findViewById(R.id.btnLinearLayout);
+        btnRelativeLayout = findViewById(R.id.btnRelativeLayout);
+        btnConstraintLayout = findViewById(R.id.btnConstraintLayout);
+
+
+        btnLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , LinearLayout.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRelativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Relative_Layout.class);
+                startActivity(intent);
+            }
+        });
+
+        btnConstraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Constraint_Layout.class);
+                startActivity(intent);
+            }
         });
     }
 }
